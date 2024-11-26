5c3b533c91fcc7f49b6f79ebd4c0b141f9471e7a
Wille4321 <wille.nordstrom08@gmail.com>
2024-11-26 08:56:34 +0200

    Add files via upload

---
+++ Variables in C# UPPG/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Variables_in_C__UPPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string name = "Wille";
+            Console.WriteLine(name);
+
+            int Num1 = 1234;
+            Console.WriteLine(Num1);
+
+            int Num2 = 4321;
+            Console.WriteLine(Num2);
+        }
+    }
+}
