16ee6fac8ada91fccf0554b97b3e4ad445fbe1d5
Wille4321 <wille.nordstrom08@gmail.com>
2024-11-26 12:16:01 +0200

    Add files via upload

---
+++ ArrayMultiplicstionTask UPPG/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArrayMultiplicstionTask_UPPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int Num = 7;
+            int Length = 5;
+
+            int[] array = new int[Length];
+
+            Console.WriteLine($"Multiplication table for {Num} is: ");
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine($"{Num} x {1} x {Num * i}");
+            }
+
+
+        }
+    }
+}
