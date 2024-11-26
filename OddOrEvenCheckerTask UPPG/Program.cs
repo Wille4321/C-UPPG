d5b88024b7f90d9b3c5d704c750f7ffc684b954b
Wille4321 <wille.nordstrom08@gmail.com>
2024-11-26 10:58:32 +0200

    Add files via upload

---
+++ OddOrEvenCheckerTask UPPG/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OddOrEvenCheckerTask_UPPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter a number: ");
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            if(number %2==0)
+            {
+                Console.WriteLine($"The number {number} is even");
+            }
+            else
+            {
+                Console.WriteLine($"The number {number} is odd");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
