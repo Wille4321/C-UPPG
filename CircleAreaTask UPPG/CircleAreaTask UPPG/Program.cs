4b708c9c621e794b8231c6606f082eaf800788e8
Wille4321 <wille.nordstrom08@gmail.com>
2024-12-04 17:00:10 +0200

    Add files via upload

---
+++ CircleAreaTask UPPG/CircleAreaTask UPPG/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CircleAreaTask_UPPG
+{
+    class Circle
+    {
+        public double Radius { get; private set; }
+
+        public Circle(double radius)
+        {
+            Radius = radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * Radius * Radius;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Circle circle = new Circle(1.5);
+
+            double area = circle.CalculateArea();
+            Console.WriteLine("Area of the circle is: " + area);
+        }
+    }
+}
---
+++ CircleAreaTask UPPG/CircleAreaTask UPPG/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("CircleAreaTask UPPG")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("prakticum.fi")]
+[assembly: AssemblyProduct("CircleAreaTask UPPG")]
+[assembly: AssemblyCopyright("Copyright © prakticum.fi 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("4d5eeb2f-9229-4caf-a2d7-778db3928479")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
