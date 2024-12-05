8356e1b927c4199f9d401c7f6074bb1d71d41fae
Wille4321 <wille.nordstrom08@gmail.com>
2024-12-05 08:45:49 +0200

    Add files via upload

---
+++ AreaCalculationTask UPPG/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AreaCalculationTask_UPPG
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the width of the triangle:");
+            double baseWidth = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Enter the height of the triangle:");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            double area = baseWidth * height / 2;
+
+            Console.WriteLine($"The area of the triangle is: {area}");
+
+
+            Console.ReadKey(); 
+        }
+    }
+}
